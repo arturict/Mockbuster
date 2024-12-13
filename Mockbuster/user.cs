f4393e125d7e02f6393d987b0b5a9f4c9ab3364c
Artur Ferreira <artur.ferreira@ckw.ch>
2024-12-13 08:53:06 +0100

    created user base code (no code within function)

---
+++ Mockbuster/user.cs
@@ -0,0 +1,20 @@
+namespace Mockbuster;
+
+
+class user
+{
+    
+    private string name;
+    private List<item> borrowedItems;
+    public user(string name)
+    {
+        this.name = name;
+    }
+    public void borrowItem(item item)
+    {
+    }
+    public void returnItem(item item)
+    {
+    }
+    
+}
