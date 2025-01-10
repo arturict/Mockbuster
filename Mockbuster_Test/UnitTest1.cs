3007e611bf5fb867b7813a6ed5c017dbe0abd37b
Simon Mettler <simon.mettler@kauz.ch>
2025-01-10 19:58:51 +0100

    Adjusted Unit Test

--- Mockbuster_Test/UnitTest1.cs
+++ Mockbuster_Test/UnitTest1.cs
@@ -1,14 +1,36 @@
-namespace Mockbuster_Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Mockbuster;
 
-[TestClass]
-public class UnitTest1
+namespace Mockbuster_Test
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestClass]
+    public class UserTests
     {
-        Console.WriteLine(); //objectName.Borrow()
-        Console.WriteLine();
-        Console.WriteLine();
-        Console.WriteLine();
+        [TestMethod]
+        public void User_CanBorrowAndReturnItem()
+        {
+            // Arrange
+            var user = new User("Alice");
+            var item = new Item("Digital Book");
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            // Act
+            user.BorrowItem(item);
+            user.ViewBorrowedItems();
+            string borrowedOutput = consoleOutput.ToString().Trim();
+
+            consoleOutput.GetStringBuilder().Clear();
+
+            user.ReturnItem(item);
+            user.ViewBorrowedItems();
+            string returnedOutput = consoleOutput.ToString().Trim();
+
+            // Assert
+            Assert.AreEqual("Digital Book", borrowedOutput); // Item should appear when borrowed
+            Assert.AreEqual("", returnedOutput); // List should be empty after returning
+        }
     }
 }
